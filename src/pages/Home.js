f897114ec531998916760620474c90a5d7f0b817
Kristóf <kristef.g@gmail.com>
2021-10-14 14:23:23 +0200

    base layout of the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,77 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Box,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Grid container spacing={1}>
+        <Grid container item xs={6} spacing={1}>
+          <Grid item xs={4}>
+            <Card>
+              <CardHeader title='Balance' />
+              <CardContent>3400Ft</CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardHeader title='Expenses' />
+              <CardContent>3400Ft</CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardHeader title='Incomes' />
+              <CardContent>3400Ft</CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title='Diagram' />
+              <CardContent>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid container item xs={6}>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title='Diagram' />
+              <CardContent>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid container xs={12} item spacing={1}>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title='Még nem tudom' />
+              <CardContent>
+                <Typography>3400Ft</Typography>
+                <Typography>3400Ft</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Home;
